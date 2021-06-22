cc99e0417c779e6d50eaacce41d4292dc9cd9368
Linjie Li <linjieli222@users.noreply.github.com>
2021-06-21 20:40:01 -0700

    fix nms error for svmr

--- utils/tvr_eval_utils.py
+++ utils/tvr_eval_utils.py
@@ -225,9 +225,11 @@ def post_processing_svmr_nms(
     processed_svmr_res = []
     for e in svmr_res:
         # the predictions are sorted inside the nms func.
-        e["predictions"] = temporal_non_maximum_suppression(
-            e["predictions"][:max_before_nms],
-            nms_threshold=nms_thd)[:max_after_nms]
+        _predictions = [d[1:] for d in e["predictions"][:max_before_nms]]
+        _predictions = temporal_non_maximum_suppression(
+            _predictions, nms_threshold=nms_thd)[:max_after_nms]
+        _video_id = e["predictions"][0] # video_id is the same for all predictions
+        e["predictions"] = [[_video_id, ] + d for d in _predictions]
         processed_svmr_res.append(e)
     return processed_svmr_res
 
